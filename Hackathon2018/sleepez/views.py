a9e233b70cd8003f6d7f4d87fece21efeba21ebb
JordanFalcon <j.falcon122@gmail.com>
2018-04-28 01:47:49 -0400

    Add JSON data for shelter data

--- Hackathon2018/sleepez/views.py
+++ Hackathon2018/sleepez/views.py
@@ -7,17 +7,17 @@ def index(request):
     return render(request, 'sleepez/index.html')
 
 
-def test(request):
-    # response = requests.get('https://data.cityofnewyork.us/resource/5ud2-iqje.json')
-    shelters_data = json.load(open('sleepez/static/data/shelters.json'))
-    shelters = {}
+# def test(request):
+#     # response = requests.get('https://data.cityofnewyork.us/resource/5ud2-iqje.json')
+#     shelters_data = json.load(open('sleepez/static/data/shelters.json'))
+#     shelters = {}
 
-    for shelter_data in shelters_data:
-        area = shelter_data["service_area"]
-        address = shelter_data['address']
-        shelters[area] = address
+#     for shelter_data in shelters_data:
+#         area = shelter_data["service_area"]
+#         address = shelter_data['address']
+#         shelters[area] = address
 
-    return render(request, 'sleepez/test.html', 
-        shelters
-        )
+#     return render(request, 'sleepez/test.html', 
+#         shelters
+#         )
 
