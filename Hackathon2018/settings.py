b45d0683c61517256981c1127b7655124cfd5e74
HaroldDLeon <harolddleon@users.noreply.github.com>
2018-05-12 10:19:13 -0400

    Added logging information

--- Hackathon2018/settings.py
+++ Hackathon2018/settings.py
@@ -118,6 +118,22 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Logging information
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
